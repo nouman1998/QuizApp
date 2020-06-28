2e591c8b437a396000cde43d4b4b5a9c373b3e89
nouman1998 <nouman.ejaz198@gmail.com>
2020-06-29 02:38:51 +0500

    commit 7

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.static(  './dist/quiz-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('index.html', { root: 'dist/project-name'} ));
+  res.sendFile(path.join(__dirname +'/dist/quiz-app/index.html'));
 });
 
-// default Heroku PORT
+// default Heroku PORT 
 app.listen(process.env.PORT || 8080);
 console.log("Listeninig");
