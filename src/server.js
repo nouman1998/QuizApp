863d335b169f1ded48d6b583238056b3d0cad825
nouman1998 <nouman.ejaz198@gmail.com>
2020-06-29 02:03:20 +0500

    commmit 4

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/quiz-app'));
+app.use(express.static(  './dist/quiz-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/quiz-app/index.html'));
+  res.sendFile(path.join(__dirname ,'/dist/quiz-app/index.html'));
 });
 
 // default Heroku PORT
